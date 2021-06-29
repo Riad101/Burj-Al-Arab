fd586541f9c2b9649ee541e4dba2c6a77858e44d
smriad <skmdriad@gmail.com>
2021-06-29 23:54:58 +0800

    firebase added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,7 @@ const Login = () => {
     return (
         <div>
             <h1>This is Login</h1>
+            <button>Google Sign In</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBw9l36SGVHCWrqn18GtZfh87HnBEORtqw",
+    authDomain: "burj-al-arab-a07b2.firebaseapp.com",
+    projectId: "burj-al-arab-a07b2",
+    storageBucket: "burj-al-arab-a07b2.appspot.com",
+    messagingSenderId: "181404264924",
+    appId: "1:181404264924:web:7a779a358b191abbc00719"
+  };
