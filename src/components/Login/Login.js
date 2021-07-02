b9d03c25b11d88a95db505b94f584b6ae0b4d132
smriad <skmdriad@gmail.com>
2021-07-02 23:22:53 +0800

    error message

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -30,14 +30,11 @@ const Login = () => {
                 history.replace(from);
                 // ...
             }).catch((error) => {
-                // Handle Errors here.
-                var errorCode = error.code;
+                
                 var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
+                console.log(errorMessage);
+                
+                
   });
     }
     return (
